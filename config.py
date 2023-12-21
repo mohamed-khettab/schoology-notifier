da5f8e858bd3117a29b7e7d7405ec7a5888491fb
Mohamed Khettab <93957717+mohamed-khettab@users.noreply.github.com>
2023-12-21 18:15:47 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+key='' # Your Key Here
+secret='' # Your Secret Here
+user_id='' # Your User ID here
+
+"""
+Get your Consumer Key & Consumer Secret by: 
+Going to https://(DISTRICT NAME HERE).schoology.com/api
+
+
+Get your user ID by:
+Going to your profile. 
+Look at the search bar.
+https://(DISTRICT NAME HERE).schoology.com/user/(THE NUMBER HERE IS YOUR USER ID)/info
+"""
